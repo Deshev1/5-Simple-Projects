0d08766b3d0b03442410132370ee9a93008e334f
Deshev1 <stefandeshev@gmail.com>
2024-07-30 19:42:21 +0300

    refactored expense tracker js

--- js/expense.js
+++ js/expense.js
@@ -1,61 +1,128 @@
-let expenseDate = document.querySelector(".expense-date");
+//Budget input selectors
+const budgetInputCard = document.querySelector(".set-budget-card");
+const budgetInput = document.querySelector(".budget-input");
+const budgetSubmit = document.querySelector(".budget-btn");
 
-let budgetCard = document.querySelector(".set-budget-card");
-let budget = document.querySelector(".budget-input");
-let submit = document.querySelector(".budget-btn");
+//Budget display selectors
+const budgetDisplayCard = document.querySelector(".budget-display-card");
+const budgetDisplayLabel = document.querySelector(".budget-number");
 
-let budgetDisplay = document.querySelector(".budget-display-card");
-let budgetNumber = document.querySelector(".budget-number");
+//Transaction input selectors
+const transactionType = document.querySelector(".transaction-type");
+const transactionName = document.querySelector(".expense-name");
+const transactionDate = document.querySelector(".expense-date");
+const transactionAmount = document.querySelector(".expense-number");
+const transactionSubmit = document.querySelector(".expense-submit");
 
-let transactionCard = document.querySelector(".transaction-details");
-let transactionType = document.querySelector(".transaction-type");
-let transctionName = document.querySelector(".expense-name");
-let transactionDate = document.querySelector(".expense-date");
-let transactionNumber = document.querySelector(".expense-number");
-let transactionSubmit = document.querySelector(".expense-submit");
+//Transaction table selectors
+const transactionTable = document.querySelector(".transaction-table");
+const tableEmptyLabel = document.querySelector(".empty-label");
 
-let table = document.querySelector(".table");
-let emptyLabel = document.querySelector(".empty-label");
+//Set default date to today
+transactionDate.valueAsDate = new Date();
 
+//Dynamic variables
 let currentBudget;
 let transactions = [];
-
-expenseDate.valueAsDate = new Date();
 
 /* ///////////////////////// */
 /* FUNCTIONS */
 /* ///////////////////////// */
 
+//Budget input function
 let setBudget = function () {
-  if (!budget.value) {
+  //Take budget from input and convert it to a number
+  let budgetInputVal = Number(budgetInput.value);
+  //Alert the user if budget input is empty
+  if (!budgetInputVal) {
     alert("Please enter your budget!");
   } else {
-    currentBudget = Number(budget.value);
-    budgetNumber.innerHTML = `${currentBudget}lv.`;
-    budgetCard.style.display = "none";
-    budgetDisplay.style.display = "flex";
-    resetField(budget);
+    //Set current budget to input value
+    currentBudget = budgetInputVal;
+    //Set budget display label
+    budgetDisplayLabel.innerHTML = `${currentBudget}lv.`;
+    //Remove budget input card
+    budgetInputCard.remove();
+    //Show budget amount card
+    budgetDisplayCard.style.display = "flex";
   }
 };
 
+//Input field reset
 let resetField = function (...fields) {
   fields.forEach((f) => (f.value = ""));
-  document.querySelector(".expense-date").valueAsDate = new Date();
 };
 
-let addTransaction = function (type, date, nameT, number) {
-  budgetNumber.innerHTML = `${currentBudget}lv.`;
-  emptyLabel.style.display = "none";
-  table.insertAdjacentHTML(
-    "beforeend",
-    `<tr class="${type}">
-        <td class="t-type">${type}</td>
-        <td class="t-date">${date}</td>
-        <td class="t-name">${nameT}</td>
-        <td class="t-number">${number}lv.</td>
+//Validate transaction inputs
+let validate = function (t, d, n, a) {
+  //checks if all entries are entered
+  if (!t || !d || !n || !a) {
+    alert("Please fill in all expense details!");
+    return;
+  }
+  //checks if budget is entered
+  else if (!currentBudget && !currentBudget === 0) {
+    alert("Please define your budget!");
+    return;
+  }
+  //checks if expense is bigger than budget
+  else if (t === "expense" && a > currentBudget) {
+    alert("Insufficient funds!");
+    return;
+  }
+  //processes transaction
+  else {
+    return 1;
+  }
+};
+
+//Process transaction inputs (after validation)
+let processTransactionInputs = function (t, d, n, a) {
+  //Change to negative amount if type = expense
+  if (t === "expense") {
+    a = -a;
+  }
+
+  //Store transaction details in transactions list
+  transactions.push({
+    type: t,
+    date: d,
+    name: n,
+    amount: a,
+  });
+
+  //Update current budget value
+  currentBudget = currentBudget + a;
+  //Update current budget label
+  budgetDisplayLabel.innerHTML = `${currentBudget}lv.`;
+  //Reset input fields
+  resetField(transactionType, transactionName, transactionAmount);
+  //Reset transaction date to today
+  transactionDate.valueAsDate = new Date();
+};
+
+//Display transactions
+let displayTransactions = function () {
+  //Display table labels
+  transactionTable.innerHTML = `<tr class="transaction-table-headings">
+            <th>Type</th>
+            <th>Date</th>
+            <th>Name</th>
+            <th>Amount</th>
+          </tr>`;
+
+  //Display transaction row
+  transactions.forEach((t) =>
+    transactionTable.insertAdjacentHTML(
+      "beforeend",
+      `<tr class="${t.type}-row">
+        <td class="t-type">${t.type}</td>
+        <td class="t-date">${t.date}</td>
+        <td class="t-name">${t.name}</td>
+        <td class="t-number">${t.amount}lv.</td>
       </tr>`
+    )
   );
-  resetField(transactionDate, transctionName, transactionNumber);
 };
 
 /* ///////////////////////// */
@@ -63,100 +130,36 @@ let addTransaction = function (type, date, nameT, number) {
 /* ///////////////////////// */
 
 //Set budget
-submit.addEventListener("click", function () {
-  setBudget();
-});
+budgetSubmit.addEventListener("click", setBudget);
 
 //Add expense
 transactionSubmit.addEventListener("click", function () {
-  //collect entries
+  //Take input values
   let type = transactionType.value;
   let date = transactionDate.value;
-  let nameT = transctionName.value;
-  let number = Number(transactionNumber.value);
+  let nameT = transactionName.value;
+  let amount = Number(transactionAmount.value);
 
-  //process entries
-  let processTransaction = function () {
-    //save negative number if expense
-    if (type === "expense") {
-      number = -number;
-    }
-
-    //store transaction details
-    transactions.push({
-      type: type,
-      date: date,
-      name: nameT,
-      amount: number,
-    });
-
-    //update balance
-    currentBudget = currentBudget + number;
-    budgetNumber.innerHTML = `${currentBudget}lv.`;
-    console.log(transactions);
-  };
-
-  let displayTransactions = function () {
-    table.innerHTML = `<tr class="table-headings">
-            <th>Type</th>
-            <th>Date</th>
-            <th>Name</th>
-            <th>Amount</th>
-          </tr>`;
-
-    transactions.forEach((t) =>
-      table.insertAdjacentHTML(
-        "beforeend",
-        `<tr class="${t.type}">
-        <td class="t-type">${t.type}</td>
-        <td class="t-date">${t.date}</td>
-        <td class="t-name">${t.name}</td>
-        <td class="t-number" lenumber="${t.amount}">${t.amount}lv.</td>
-      </tr>`
-      )
-    );
-  };
-
-  //validate entries
-  let validate = function () {
-    //checks if all entries are entered
-    if (!type || !date || !nameT || !number) {
-      alert("Please fill in all expense details!");
-      return;
-    }
-    //checks if budget is entered
-    else if (!currentBudget) {
-      alert("Please define your budget!");
-      return;
-    }
-    //checks if expense is bigger than budget
-    else if (type === "expense" && number > currentBudget) {
-      alert("Insufficient funds!");
-      return;
-    }
-    //processes transaction
-    else {
-      processTransaction();
-      displayTransactions();
-    }
-  };
-
-  validate();
+  //If validation is successful, run process and display transactions
+  if (validate(type, date, nameT, amount)) {
+    processTransactionInputs(type, date, nameT, amount);
+    displayTransactions();
+  }
 });
 
 // //Remove expense
-// table.addEventListener("click", function (e) {
+// transactionTable.addEventListener("click", function (e) {
 //   if (e.target.classList.contains("remove-transaction")) {
 //     let transaction = e.target.parentElement.parentElement;
-//     let transactionNumber = Number(
+//     let transactionAmount = Number(
 //       transaction.querySelector(".t-number").getAttribute("lenumber")
 //     );
-//     console.log(transactionNumber);
-//     console.log(typeof transactionNumber);
+//     console.log(transactionAmount);
+//     console.log(typeof transactionAmount);
 
 //     console.log(currentBudget);
-//     currentBudget = currentBudget - transactionNumber;
-//     budgetNumber.innerHTML = `${currentBudget}lv.`;
+//     currentBudget = currentBudget - transactionAmount;
+//     budgetDisplayLabel.innerHTML = `${currentBudget}lv.`;
 
 //     console.log(currentBudget);
 
